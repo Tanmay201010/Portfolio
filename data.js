cfc810f52a94ca6d0b22440ed84cef8e38bafc19
Tanmay Jain <140507915+Tanmay201010@users.noreply.github.com>
2026-07-17 17:52:27 +0530

    Update student database (Aetheris CMS)

--- data.js
+++ data.js
@@ -22,139 +22,147 @@ window.students = [
     "id": 1,
     "first_name": "Tanmay",
     "id_number_hash": "200b75b5d21fb01e0239c422481838daa0ec9b7259627c5ad69a98132ae6d803",
-    "encrypted_id_number": "9221734dcd60b400568f9dbf42106687:904c542e25a29e84cc1b7566:5fb5a61471bdaa35e4d09048fe8cd9195cb6544aa125f3cb",
-    "encrypted_observations": "30c561a2bac00df3dd8d62cb0f261484:cd611fa15a955f4b5ae5bb10:97ff0d238e510b23e7ac06e7eb99c3fc7d660b1263cbe7a8af33c546f30448ec79d5e3a85a8f682c42633d2b2f7f2a0114258b583a08bd3849434ad71924f7aca0894ab8c3e55507d9325dbd7eef0008664b11d7672e4385929ba6dba21094bc8aa5d2aa6e9a8738d0e1298738d1f490399f842ed406f4ccda988cac91fd61e3cf5211f7df412cfa"
+    "encrypted_id_number": "6039c3561ce5d411ea5231c01855a16b:ad8879aea411959c36452b6d:8aa5eda2d368890878b36cdc4f628ccd61e803c7dc19b708",
+    "encrypted_observations": "2174f7d6ca2af632627c07d2c7478e57:e66510042d1f64bf2faddbb7:255cf40ce50e738aca6106feb95d7ef538c81d329319f0d92a94cbfdaae43352a6ca861b0733b38a20b5364475bc5cdb2ba2e7fef6395e6f5ef7d33a99536c33f0f1268f1217e4cc43335a6b3dd8141e6e8a1fc5db646d7926a69e9aaf3961dee5f0e8d943069c947108ef21f2270503ebd7fc685ac8d8972cba3abf693137300ac5b6cb3b6ac7da"
   },
   {
     "id": 2,
     "first_name": "Saalim",
     "id_number_hash": "13c0f62212575ee4fc665be8b5d008bb39a6c6a57d4999a5eaa0aaba07ee3b84",
-    "encrypted_id_number": "66a3f7a8856d911bceacfa1594eb5098:286d5e3926b13d8d181b7ed9:a1880b1fb1b3660848dfb2295eb46eaf75d072fe5dd50231",
-    "encrypted_observations": "cfd1fa36a85c5667b1818eb6eb5baad0:950f50d3203ef17ba64c7bdc:0a7506822e07799ca5dbd82a85dbf0d432c7e91f195aa81a2dbb758ea2fc60e89a5829588d5e2f3a4716ff6ded15eec5f1ff0bd7ae33919785778f6d90bace93e8a888c34567fb9c94b77f8944dbc8c53eb045f4b66e6685f23bb00ce89b4c826a599308a17652bc1755d6e87690fb6ed5ceab14c09a18d22cef2428a9"
+    "encrypted_id_number": "684c5eecff5922451e3bb3109b6ff273:46119ee35d3ec068e3b9ddd9:f2d07be6cf9dc83975bbb327b04550ad220b48acce3fa920",
+    "encrypted_observations": "eefea50b7ea3fba0433941d41a273f10:be2001c44405d13189df574f:adfd832badfac19cff4e7f4a3ebd1042db0d2a091954b226893783cbf31387372cf4c4b7d8e4ac78b435e0f810d10c675d4af9a0dd9bb43e35849ba0905fa8fc4e0c0a1f830fd6273368048829404cf7587327cb734046585011bea6645d82753cc0c058c677d98f0c9dd55161e1c2644c3066fbd402302ee1f591a919"
   },
   {
     "id": 3,
     "first_name": "Dhruv",
     "id_number_hash": "a96f27f6fc1073ebe45f1b7a6daa14af11053a2ae4ae664a31b00d5823573a4c",
-    "encrypted_id_number": "ee8f5fe7a58d95a288bfed78905c948f:d30d90828ea6b26c6f636243:2972c410712c43c520bc6dd052e0fe5541dcf08845b3a282",
-    "encrypted_observations": "770c7930aaf966c1da13f6b349ac32ac:3b30a680ee3e26d4a1acb1a0:734903575d9f9523ad7454ce8fd3ceba0c33f4f814ffa803d76698fde5a782c0946b9253da888fe9133a601fa0254e7c530457bd74301c3a3f4023cefd03811f3ae68305a6724488bb1a1d0c7ad73da6f174d408c4b5c611c451f95b36146b031a7254860825597b954480bd6c30afc134accd5e1897983e40511fc6aba8dca4b623b4ebbd1aa4f46ad9af529ef9719a3901d6b4b5b933e361fd4b69e69e389d7306ebb3d756023c4086a321094c5f0cc180248afd25b296edc4e094e685a513d82636e4e8a0d222305613a9c1e2b99306c282480caa11dc3ece507abfdf5c71610cb42f184157eb589b6183ef1ea577f055ae0b6325880405bfb7e292e0c37dea3786c2a5"
+    "encrypted_id_number": "396a9d93dd1f031642de2d855d081844:609288639139851f7431ccad:cdb3d9e272fdc52b5cd03a6b3783f011bc53730947ddeea9",
+    "encrypted_observations": "8e902b2b3d22cc01c17012630c834fa6:bffbd826d85eee3b168e9899:662f9919647f77e2a6121b986941404fbb6c585c0e3542b8e50e05dda38877e4b15c4e49dafe547729b711bc6dbe913586836c0587272ae5bc3a3c4469d4e7da22a0d82ae3a0d6d9c8b028ea791e4e953087ecad9cf7854aff1e26aa2a1e3d50401120db66d050cb1e8dfbd94cf21593e996119b60aee88765def90368d68d21bdd3924346759e6e7a5ab7e15cd6279d0739e77fbfb63ca47f476b54df2d41780dd6dfc420f53b48aa861ce48505626225c876dacb5b2ad5253af5cab54fd93eb4e3a4bfe7d3eb0dae9e6b1c82978944590cca5bc42b01bf356b0f2d01c4d428a61aeb8f6b5bb5703482675b8a29cdf8a93d1da9f7e71926961621e2206bddd9b5fe6ecbe9"
   },
   {
     "id": 4,
     "first_name": "xyz",
     "id_number_hash": "108f02fc913a0847fddd5cdb44681b3eec6b15b359f1cf7407639d708efb3a72",
-    "encrypted_id_number": "fa7486ddd52f3dd53bd3278479f8afef:8201987de67c8f33ad186f39:ec5d070f329de0cb94c2123f9309f6ff66ece984725943",
-    "encrypted_observations": "4123aeb049691988810a2cabbbe07d19:e4594ed979d6618eb71d01f7:d3739fdba71885856291b56221188839f2f4ece608d388f5df8f0dd74a850a7bdf8c524c1142d21db605db1c5aa364361c50cbe00afe20d016bd314979301bf760c4"
+    "encrypted_id_number": "f3383bb2222db12d21625cfd931e3019:d54279d9def3d11f2080c5e2:719afeba124eead4777e510810328d505041921abcf397",
+    "encrypted_observations": "2e5585d8d8d3c3a030ce1fdbe77f0ce7:96e466a256e52a8f36c2dcc5:1657fa840c09d5d7670b17e2f48cf00c3124936d0fdfaf2fd4a50f776556f76fedfcad3943f413d147ce8b98ce4db9b919ee5addfb0dd57efe085d283aae508b60ef"
   },
   {
     "id": 5,
     "first_name": "Mehull",
     "id_number_hash": "2df9f32993401c7b07d34f3d4c22e129432a94e9db1ab35d75333bd6b1cd35a3",
-    "encrypted_id_number": "3f46d4ba9bdb79b0653104e0e90b2b4e:54aed18bfb790a9fe80514e9:7d7de0bd112c451dfe061ab4a7d76ab9de4e4e867163e7",
-    "encrypted_observations": "46620c306c3ac31ecbceada2cf873396:132fd09b4ee7344f624565c7:57b00e93ec51ff2afbbd7cd423fc43566b9b42f991334d87141d9d7c24e5a88ee5b6c59b970f7a7f34e2f372cfc506dfb866cd253c15e38887aa7614ed69447a"
+    "encrypted_id_number": "50cbdb458455ba1c8b362308dd4fc9b3:da5cc6102f1bee1606043bd0:636122d2b3d6f6c2cd11ba00bcd80464c1add535f78e91",
+    "encrypted_observations": "ce9ba96db30f3f9266df8bf4e9c10b87:410013ef0d29a0c94b3b9ff8:49a061b8e7f5f1bbf872f0be19be0464505be0362c81668e5d38a5a95ff83c8bbd723e42763737dc70f707562e0e0f9aaaba03c826189fa77f7fd97788026144"
   },
   {
     "id": 6,
     "first_name": "AARUSHI SONI",
     "section": "11 K",
     "id_number_hash": "00211c78ef909b0b45d56e49a53e814dc514b72f6316d7f0ca9298ef01a483e0",
-    "encrypted_id_number": "ef15c221ae3afcf45a85ff1b16c4a129:0c37ba4bd43bfce52da0e3b4:2b0a1fa19dd43cb42390aad2a3d8bfc0d9be557faea7b5",
-    "encrypted_observations": "a8851e5b204d91a9aa32ef5916e4a8a1:122602127abb147f40feb5d9:49de413cb0c9403188abce71b051071f3438"
+    "encrypted_id_number": "a52e849cbde6b62dbc486ecb2b8bf34e:09969a646bd7594a1547b086:02b9b7700a2cd23e116f19e2f40af170f92bed7b881283",
+    "encrypted_observations": "7bf0e05e905b2388eec5c3a6654127e7:30b54b50d03eee84731216e4:2b0865e26dbd0b97f94de02c0462b9af5885"
   },
   {
     "id": 7,
     "first_name": "AASTHA AGARWAL",
     "section": "11 K",
     "id_number_hash": "e99a6334c95b86244d7380213ba3b867e0543750ce640a7b8bd852cd286b9f39",
-    "encrypted_id_number": "098d4b0720786c5fe3710f1111b108ba:5fd2ff8902f28ebdb3076880:d1796bc3048e12066f0b7524290032ea788e313ed93c6d",
-    "encrypted_observations": "5e0f1560063fa8ef3e94a93226f44687:76dbaae1e07b070ded45aa05:c4cadcd4119244a65ff799f97d8c66fc5ee0"
+    "encrypted_id_number": "0a9548b6399aa407b39e45c8c1e98ee7:198f955aa195d99ab162058a:1d77fd864c34180e2609ecf852add2acd2f0e4a56a2b09",
+    "encrypted_observations": "cb11b223d807b767df2eb2ca4bfde70d:bcc25955e8757d1c70dbcd0d:d12151be78562fb35e8cbdce18258fbc4a5f"
   },
   {
     "id": 8,
     "first_name": "ADIMULAM MANASWINI",
     "section": "11 K",
     "id_number_hash": "1de6d2291e6f5b3a2a88c16746ee61c8a11702f44e393d187aba42ac1cbc80cf",
-    "encrypted_id_number": "01f11298dc341552e41f709b1f7e1a86:2391856db93816e5b27adc26:614fb09d92dbee0b6eb08e9c696b737cbc6bc691493851",
-    "encrypted_observations": "d794b7163e2f76011bbf060757e31e53:712b027efff15a2ded8379ff:8785ee99019b1bca1060532262effdb6532e"
+    "encrypted_id_number": "b7473673860070ee13728070de777797:9ad7287fb40f04b65d567b6a:bb99323b5363f3a6faa7456fa0142a43fea9e4fef7299f",
+    "encrypted_observations": "158284884af44591b4e86a9f0ebada3a:c44008ba8999925d9b90c3b4:d6656a9929c4a01b50debd9bc616181c6b94"
   },
   {
     "id": 9,
     "first_name": "AKSHITH A BORDIYA",
     "section": "11 K",
     "id_number_hash": "dd8a496b27b1c06b912d37f6e142dec75b1797ccb2a612a97fec32eda7d5e5fa",
-    "encrypted_id_number": "c5df5fee5fbf34866525b478cdcf11f9:a94a1b369e58cd049985ebf0:a2445b6a6f6c718af7ceaec89a7c2e9cb760a5cf7ec659",
-    "encrypted_observations": "efb6fdca08f1964d24fd3773846b2d8f:46bbdaa5410e897e7b5d94e1:7fc7cf507f1dce99467079b58e7215af7d91"
+    "encrypted_id_number": "b28a581930c0fcb0da5d79c75fe57260:ac9e0649b02352d8d2eccb93:cbe35032f797355caddddf26db168d1e764be1c373e8ba",
+    "encrypted_observations": "03c2eaf520cabf688778d2cff5033413:1d87cdfd4c05f163320c9583:7f897100ccbce78e3f9447d808051c31225d"
   },
   {
     "id": 10,
     "first_name": "ARUSH PAHUJA",
     "section": "11 K",
     "id_number_hash": "0ddb29c2ece50cc87274319853f90756abe01e74f614267d56a50736aa25a995",
-    "encrypted_id_number": "9cf4a76e049526550837ddfdefce1b22:4385ec2c4920f8185cf3fa97:c4477c080c4d9e2bf3b88b7e7e3a952078ac80cc3da4c2",
-    "encrypted_observations": "2fb860c277415b4e9fbb86d8aab54bf3:72521f06bc6414256b6ef783:94410adce8018e85e9d71b596fdf2cb8c427"
+    "encrypted_id_number": "bee0e00fd9231d80c7b1866ee02aa7b3:4d19c6ea0d0ffdd671a475c1:cce01c48d9d94b9239498211f8a8e8ae9190888fadc40f",
+    "encrypted_observations": "bd9f42c6efc4bd004d1a90e154e3b2e2:fd2ba3a2adad27305689e018:d86bb65a1974288584d8ecdc5690d72afde8"
   },
   {
     "id": 11,
     "first_name": "ATMAKIRTEE",
     "section": "11 K",
     "id_number_hash": "6303654371dfdf0bd24ccd7b0bf7684c3140317991f57798317941bcd537e0ec",
-    "encrypted_id_number": "b378c08653dd072e7543ef47fe79195a:aa2664d6cf7a9ec8a595925d:78fd7d58a162282b692a3fb68102e116d4613b7416280c",
-    "encrypted_observations": "1e861ff2cbd94c60e9d98b98e08c1bf8:f7b749e9f88491ede8c61272:7d13e6d49947b61d75a5161aa421b0721f87"
+    "encrypted_id_number": "382c2708a12ba7cefdd62fae2af27353:3fc59deedda05f136563375a:8380b36db603b8ff42856a931e2a2d7f1293f97cde5932",
+    "encrypted_observations": "acfbe83a67cb08766c4351f479f93ed7:b832547d7d65c406d01c81c1:a84d9a523563380f82ee501a455980286fa8"
   },
   {
     "id": 12,
     "first_name": "AYUSH BOTHRA",
     "section": "11 K",
     "id_number_hash": "bc497e5b04da4102c3cee5a40211056b03a6c4cdc2850fdabe5a9706caa5ff58",
-    "encrypted_id_number": "0343495603519d4e57f9d5f817d0b18a:e02eed29cb83c38fa685fecf:2cc87a594984a1ebfbc374444ab518996e214bb272053b",
-    "encrypted_observations": "47934e40e32e604807016392376cc4fa:67050d6d22a65a0021b70007:8f04a54944dbe9b824c36bc30a398eec95f4"
+    "encrypted_id_number": "ea73902ae1d0de2453298f63a6b55693:c990757bf9fa59f729d31dd4:fe7ae2a61836a4abf1e2f36433c113ec5a1a5b3be8a4e8",
+    "encrypted_observations": "1b0f710982ff1d967ca4e740d209565f:6e465fe25ec660411654104a:6487e9cb2b186637c4a86ebb3fc45d4a6d8b"
   },
   {
     "id": 13,
     "first_name": "BHATIA MEHER KAUR",
     "section": "11 K",
     "id_number_hash": "955495b193739e85da0fdbb4ec87d35f05a49c342af07e7becf3806bef21300c",
-    "encrypted_id_number": "ca212487bebfb7f7fb2efd68faadbfe8:d3f2451afab2074ec455c95b:6fbcca16955f0033288ec3cfd14c3b4f47b9d647f48be2",
-    "encrypted_observations": "f819ddc8eaae72493c043047b42d9216:3d7c44c85ddfaa684dbb31f7:c6fb1b858fbf48ad28ceb15b98e719f3dcd7"
+    "encrypted_id_number": "324f4ce3ae14669558ad8255889f1503:e36f02be4bc7fa55b2faeb67:9614a79c1de63b78f7407eac8c82d7f0e0caf1fcda4d93",
+    "encrypted_observations": "8326dd7097502c6027902a3b0b08e7ac:01c38a6bfc67359fa554ddf5:e47fd1ea922a0c4f1f69f9220c3d8c5f799d"
   },
   {
     "id": 14,
     "first_name": "BOLLAM PRANAV TEJA",
     "section": "11 K",
     "id_number_hash": "9feba269498e70aa7c24425658d2ba6cd0eba9d4e726c366c4236e9086c3a3f3",
-    "encrypted_id_number": "6912a902863bc0e829fa7ed5ef67ce7c:1b602ffd7b07a718b46ed017:f98a0e93fd165fecd2ffdcb5cb64d67bc78890bc73a110",
-    "encrypted_observations": "e002229c8bdf13b1d239d10be4f3c22b:fca70aff4a59430f4ff4b1dc:d6ac489cf150e38faf938ae0f74c22fa1209"
+    "encrypted_id_number": "e16ae1708137e512f5c7868bfdda5e7a:ce1c638c84dee5a51eb2ae97:2cc989a4c8c7990c1e0fc90d9ad2c2bd295657ce03f607",
+    "encrypted_observations": "13fa9604882f9cfb1aa1ed4b86e16985:ef52ef473a3eb0b8b86d8e75:a0b70b70580ca13c02502982bd199cfb9c44"
   },
   {
     "id": 15,
     "first_name": "DEVYANSH TATED",
     "section": "11 K",
     "id_number_hash": "8bafa3785aecd50ddade0b76b366570e0cdcb6cc73ce82f307f6c9f90ce3a5ac",
-    "encrypted_id_number": "39546d4196ef82d755f49bbfb6796e26:4b187d37f5450ddd82336c66:9a3edb764ee4209f6e76f6b701ac727aea577e4d716bac",
-    "encrypted_observations": "969342cbdbd5bac8b571f11eb95f8877:dde73863a0566a52f99e6f1a:6b878a475b9d850286fe8260a6f92f3f0b6f"
+    "encrypted_id_number": "d6d186c9cb44ec4767836c893dc32638:7686fb3314cb77c764177d94:b62504b823de90d98061e5c30345387d2ee26968bd89b7",
+    "encrypted_observations": "879683e2458c9976be72aac78e327bc4:3e78ecbf217f895c4d272197:4176331ff0cf39b6fe8800b106dd6599981a"
   },
   {
     "id": 16,
     "first_name": "DIVENA DHANSHANKAR",
     "section": "11 K",
     "id_number_hash": "6448c31bd27b8072554132fcfc8c404d31c5e7472ea95597b386174f637a5bbf",
-    "encrypted_id_number": "eb8cdee49d20290587ecfe27172e70c8:91c2db8f1959a731ff9969af:0d1536b2831470aca8ac9172da1ede9e89880a82d0d2ce",
-    "encrypted_observations": "23973fdc733b5681d4315cc18fcde97b:adf1f57a9120402458dd586f:e4d959dd96dd2ac95f2921fbe41644ab68f0"
+    "encrypted_id_number": "3ff7cc198e1a109f0f824ba1ef113be4:6038dcd8b6c36fcf25dfe772:983550ce5e41a558413a67cc003ce565bd98c46b2004b7",
+    "encrypted_observations": "4d4f98df7c7c3d90fc4138dd283248d4:bb8d4f846935ba385dce3a0b:43853436048b8ea47f5f1c5f9eefa25b466c"
   },
   {
     "id": 17,
     "first_name": "DIVIT VADDEMANU",
     "section": "11 K",
     "id_number_hash": "284d5a657fe6a4441407495e9a42abe9e714322e3b729bbd047ab72a230aa2ab",
-    "encrypted_id_number": "5e7c44ad43aa670d396eb6e6869b04dc:68db84d8d86ce48897cb69b0:5c52a14272e2f95c0d13ea4b8c153e68ff965c186ec7ce",
-    "encrypted_observations": "de75991febe6948859701a7bd349069c:ae81f0236663a845b6bbec7b:423d2a7fdf803da9564ba868f30a204217a5"
+    "encrypted_id_number": "8c966692ab5fd382dadc6e792a7ee088:a66b0bdf29a9d374207b81af:2d51a1331224595d3c5eada002b48c17b21c5552ca3f1d",
+    "encrypted_observations": "19069855448830a8745d73c6363052ed:a9c991c12f821f852e5a8e84:6eb72ebb2e4e030a2055e7f898984a65d76b"
   },
   {
     "id": 18,
     "first_name": "EDULAKANTI PRANAVI REDDY",
     "section": "11 K",
     "id_number_hash": "2165bce82ff049ae9c32c753df551e1bc3d700f7f01c172c0d06688503c89524",
-    "encrypted_id_number": "80250cf3c74379961081084e889d9c6b:28d8d4f0a707790a161872e8:6971b52c74ac12309116dc0e64f1104556ac2f3a070365",
-    "encrypted_observations": "e8bac5bca77e0ac66c9c73e3b646813e:2415a11a52dffb33fadaf267:29a60da19578209e8ad039afdc4d1a09b834"
+    "encrypted_id_number": "5b5fca73a67b296b6bd74ac5316def0b:34f5827096a07066880ba0b8:b2cc3de8e136644670ccdcd039e4ed9233752722730aa4",
+    "encrypted_observations": "ecb04c65ab0d3174a1208ac0e2268424:fb631cea582e08caef02689a:b77a51ecba52ae207d5943826557ebd4a837"
+  },
+  {
+    "id": 19,
+    "first_name": "GADDAM YASHMIT",
+    "section": "11 K",
+    "id_number_hash": "78c7597d3841de8dc89777aeaecae29513a56433a5ed1c5bfa18ae2b9ac055b6",
+    "encrypted_id_number": "608c890262926cb52cc654f17a48a248:ee1b1d6931150d261f1b6b64:98c0d087cdee756465e7ecb50753f668677fb137e02b1c",
+    "encrypted_observations": "b69df0c997c25b4c224402522f9c003d:90660921335c330b9303f26f:a7dbceac9397f0c91447c4f43cc79af27c6b"
   }
 ];
